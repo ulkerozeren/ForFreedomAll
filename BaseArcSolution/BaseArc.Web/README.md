764df4da6a69e0ffc3073875443046b8ee5b0940
Ufuk Ozer <ufuk.ozer@turkak.org.tr>
2022-12-15 10:48:53 +0300

    Web Projesi Eklemesi

---
+++ BaseArcSolution/BaseArc.Web/README.md
@@ -0,0 +1,3 @@
+# BaseArc.Web
+
+
---
+++ BaseArcSolution/Web.Client/src/app/login/login.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+
+
+
+@NgModule({
+  imports: [
+    
+  ],
+  declarations: [
+
+  ]
+})
+
+export class LoginModule {}
